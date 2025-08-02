324c4bbc7eacacc5e635293e64068d8804aeecbd
Vimalraj-VB <vsvimal1708@gmail.com>
2025-08-02 23:26:32 +0530

    Rebuild: fix GitHub Pages 404 issue

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5738, hash: '94ce29bc12786b7a830287c7568a9a089ab1caf6fd5532c07578c1f14243e9cc', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1472, hash: '8bcc3496ca66dfe8ee34f0087736f256884ac91aa07ce70fe5ca655f1bd03bbd', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 83159, hash: '02e27f28eca60c652b851e232424e66a41bacad108f88ef2381d7b23ca1b47d2', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5738, hash: '67ea9cf9260b7f7a428669669bcdce32cfc2c590ae039d791689452814560805', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1472, hash: '0fef1f69ae8443e36651f814a4b5c14a0ae4c8988fe243c313019b0d439f720a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 83163, hash: '4a01261b3fc9f1b5c4a79c91756d095d88a9679c096579c85c171370faf2de77', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-RZS2VWZP.css': {size: 342132, hash: 'KGeiFVzwt4M', text: () => import('./assets-chunks/styles-RZS2VWZP_css.mjs').then(m => m.default)}
   },
 };
