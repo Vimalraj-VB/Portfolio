84323ee9b6ebcddf8f3869eedb3d3c142ba7587a
Vimalraj-VB <vsvimal1708@gmail.com>
2025-08-02 23:34:13 +0530

    Fix: move browser build to root of docs for GitHub Pages

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5738, hash: '67ea9cf9260b7f7a428669669bcdce32cfc2c590ae039d791689452814560805', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1472, hash: '0fef1f69ae8443e36651f814a4b5c14a0ae4c8988fe243c313019b0d439f720a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 83163, hash: '4a01261b3fc9f1b5c4a79c91756d095d88a9679c096579c85c171370faf2de77', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5738, hash: 'a982f5bf02a3fa3ea73e0d9061404863b211639f9fc5827547ba46905fc41e51', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1472, hash: 'e16fe407e72c4abcac192555fbf621ff479569b0461d58846f4aa747eae90100', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 83160, hash: '95748e047d02e572fc06751670e4ccfc40e6a123416ccece7884804d656b2671', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-RZS2VWZP.css': {size: 342132, hash: 'KGeiFVzwt4M', text: () => import('./assets-chunks/styles-RZS2VWZP_css.mjs').then(m => m.default)}
   },
 };
