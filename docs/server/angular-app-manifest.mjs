2a455c31e1947eec576bb778688ae044a8d17dd8
Vimalraj-VB <vsvimal1708@gmail.com>
2025-08-02 23:25:39 +0530

    Rebuild: fix for GitHub Pages

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5738, hash: '069704a8549d883b8a400d32616aa4b45a9fa15082a0f6b4b9d899d1111e2301', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1472, hash: '05e3b5722c7f743c84f8547253da70f4691ff7c2455a3c71dff298f638ef78eb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 83160, hash: '69c19f7b81db0092711f9d0e211db809b0173fa40d51cdd7b34539eb0e3f2d9c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5738, hash: '94ce29bc12786b7a830287c7568a9a089ab1caf6fd5532c07578c1f14243e9cc', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1472, hash: '8bcc3496ca66dfe8ee34f0087736f256884ac91aa07ce70fe5ca655f1bd03bbd', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 83159, hash: '02e27f28eca60c652b851e232424e66a41bacad108f88ef2381d7b23ca1b47d2', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-RZS2VWZP.css': {size: 342132, hash: 'KGeiFVzwt4M', text: () => import('./assets-chunks/styles-RZS2VWZP_css.mjs').then(m => m.default)}
   },
 };
